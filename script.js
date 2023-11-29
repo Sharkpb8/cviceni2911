3314910eeca56a7f1780372ee11d7a2ddd1a1d2c
sharkpb8 <sharkpb8@gmail.com>
2023-11-29 09:12:38 +0100

    pivot fix

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-const array = [1]
+const array = [4,8,7,6,9,8,1,7,2,1,0,];
+const quicksortarr = [];
 
 function numberadd(){
     let x = document.getElementById("numb").value;
     array.push(x);
     console.log(array)
+}
+
+function quicksort(){
+  let pivot = array[Math.floor(array.length / 2)];
+  let leftArr = [];
+  let rightArr = [];
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < pivot) {
+      leftArr.push(array[i]);
+    } else {
+      rightArr.push(array[i]);
+    }
+  }
+
+  console.log(leftArr);
+  console.log(pivot);
+  console.log(rightArr);
 }
